bf36737cff5f851109448a3ce54c25656dddbb13
Mattia-Carcione <mattia.carcione01@gmail.com>
2025-11-19 16:19:20 +0100

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import { BASE_PATH } from './src/const';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://mattia-carcione.github.io/pokedex/",
-    base: "/pokedex/",
+    site: "https://mattia-carcione.github.io",
     vite: {
-    plugins: [tailwindcss()],
-  },
+      plugins: [tailwindcss()],
+    },
+    output: 'static',
+    base: BASE_PATH,
 });
