d56dace763b91d204594b11053096f3255a97665
Mattia Carcione <133814979+Mattia-Carcione@users.noreply.github.com>
2026-01-27 13:57:27 +0100

    Refactor GetPokemonDetailUseCase to use services

--- src/modules/pokegen/application/usecases/GetPokemonDetailUseCase.ts
+++ src/modules/pokegen/application/usecases/GetPokemonDetailUseCase.ts
@@ -3,7 +3,8 @@ import { Pokemon } from "../../domain/entities/Pokemon";
 import { IGetPokemonDetailUseCase } from "../../domain/usecases/IGetPokemonDetailUseCase";
 import { ILogger } from "@/core/contracts/infrastructure/logger/ILogger";
 import { IPokemonRepository } from "../../domain/repositories/IPokemonRepository";
-import { safeFetch } from "@/core/utils/async/SafeFetch";
+import { INavigationPokemonLoaderService } from "../services/contracts/INavigationPokemonLoaderService";
+import { IEvolutionSpriteEnricherService } from "../services/contracts/IEvolutionSpriteEnricherService";
 
 /**
  * Use case per recuperare i dati di un Pokémon specifico.
@@ -11,6 +12,8 @@ import { safeFetch } from "@/core/utils/async/SafeFetch";
 export class GetPokemonDetailUseCase implements IGetPokemonDetailUseCase {
     constructor(
         private readonly pokemonRepository: IPokemonRepository,
+        private readonly navigationService: INavigationPokemonLoaderService,
+        private readonly evolutionSpriteEnricherService: IEvolutionSpriteEnricherService,
         private readonly logger: ILogger
     ) { }
 
@@ -20,20 +23,13 @@ export class GetPokemonDetailUseCase implements IGetPokemonDetailUseCase {
      */
     async execute(input: string): Promise<Result<Pokemon[], Error>> {
         this.logger.debug("[GetPokemonDetailUseCase] - Esecuzione del use case per ottenere i dati del Pokémon con input: " + input);
+        
         try {
-            const data: Pokemon[] = [];
             const pokemon = await this.pokemonRepository.getDetailAsync(input);
-            data.push(pokemon);
-            const fetcher = async (input: string) => this.pokemonRepository.getAsync(input);
-            const idPrevious = pokemon.id - 1;
-            const idNext = pokemon.id + 1;
-            const [prev, next] = await Promise.all([
-                safeFetch<Pokemon>(idPrevious, fetcher),
-                safeFetch<Pokemon>(idNext, fetcher)
-            ]);
-            if (next) data.push(next);
-            if (prev) data.push(prev);
-            const result = data.sort((a, b) => a.id - b.id);
+            await this.evolutionSpriteEnricherService.enrich(pokemon);
+            const navigation = await this.navigationService.load(pokemon);
+            const pokemonArray = [pokemon, ...navigation];
+            const result = pokemonArray.sort((a, b) => a.id - b.id);
             return new Result<Pokemon[], Error>(true, result, null);
         } catch (error) {
             return new Result<Pokemon[], Error>(false, null, error as Error);
