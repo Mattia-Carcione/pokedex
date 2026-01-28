9605b6ba3b8f63af4a1270575a525ef580db53fe
Mattia Carcione <133814979+Mattia-Carcione@users.noreply.github.com>
2026-01-28 18:42:44 +0000

    fix: cambiato environment di ambiente

--- src/app/di/AppContainer.ts
+++ src/app/di/AppContainer.ts
@@ -51,7 +51,7 @@ class AppContainer {
  * Istanza del container dell'applicazione PokéGen.
  */
 export const appContainer = new AppContainer(
-  !import.meta.env.DEV
+  import.meta.env.DEV
     ? EnvironmentEnum.DEVELOPMENT
     : EnvironmentEnum.PRODUCTION
 );
